4ab52d1a4690d43c0c15efd79c4e04f6ddbd9cbd
Danny <madann@berkeley.edu>
2021-03-08 21:31:14 -0800

    i changed a few things

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -35,54 +35,35 @@ const AboutPage = () => {
         </LayoutWrapperTopbar>
 
         <LayoutWrapperMain className={css.staticPageWrapper}>
-          <h1 className={css.pageTitle}>Experience the unique Finnish home sauna.</h1>
+          <h1 className={css.pageTitle}>Experience the Convenience of 3D Printing.</h1>
           <img className={css.coverImage} src={image} alt="My first ice cream." />
 
           <div className={css.contentWrapper}>
             <div className={css.contentSide}>
-              <p>Did you know that Finland has 3.2 million saunas - almost one sauna per person!</p>
+              <p>Did you know that people in your neighborhoods own 3D printers at home?</p>
             </div>
 
             <div className={css.contentMain}>
-              <h2>
-                Most of the Finnish saunas are located at the homes of individuals - indeed, most
-                people in Finland live in an apartment with sauna in it. In addition, lots of people
-                have lakeside summer cottages, which also typically come with a separate sauna
-                building near the waterfront.
-              </h2>
-
               <p>
-                To truly experience a Finnish sauna, you need to look beyond the public saunas, and
-                instead visit a real home or cottage sauna. Saunatime makes this possible for
-                everyone. All our saunas are owned by individuals willing to let tourists and other
-                curious visitors to enter their sacred spaces.
+                We have 3D printers in Berkeley, CA! They might be in your neighborhood!! In addition,
+                more 3D printer owners will join the 3DShare base. You deserve the convenience of 3D 
+                printing!
               </p>
 
-              <h3 className={css.subtitle}>Are you a sauna owner?</h3>
-
               <p>
-                Saunatime offers you a good way to earn some extra cash! If you're not using your
-                sauna every evening, why not rent it to other people while it's free. And even if
-                you are using your sauna every evening (we understand, it's so good), why not invite
-                other people to join you when the sauna is already warm! A shared sauna experience
-                is often a more fulfilling one.
+                To enjoy the low-cost convenience of 3D printing service, you need to look beyond makerspace, school labs, or other fabrication spaces, and
+                instead connect with a 3D printer owner near you within walking distance! 3DShare makes this possible for
+                every customer. All our 3D printers are owned by individuals willing to help you print your coolest 3D models!
               </p>
 
-              <h3 id="contact" className={css.subtitle}>
-                Create your own marketplace like Saunatime
-              </h3>
+              <h3 className={css.subtitle}>Are you a 3D printer owner?</h3>
+
               <p>
-                Saunatime is brought to you by the good folks at{' '}
-                <ExternalLink href="http://sharetribe.com">Sharetribe</ExternalLink>. Would you like
-                to create your own marketplace platform a bit like Saunatime? Or perhaps a mobile
-                app? With Sharetribe it's really easy. If you have a marketplace idea in mind, do
-                get in touch!
+                3DShare offers you a great way to earn some extra cash! If you're not using your
+                3D printers very often, why not run prints for others while it's free! Don't let your printers sit 
+                and sleep! Contact us for more information! We would love to have you on board.
               </p>
-              <p>
-                You can also checkout our{' '}
-                <ExternalLink href={siteFacebookPage}>Facebook</ExternalLink> and{' '}
-                <ExternalLink href={siteTwitterPage}>Twitter</ExternalLink>.
-              </p>
+
             </div>
           </div>
         </LayoutWrapperMain>
